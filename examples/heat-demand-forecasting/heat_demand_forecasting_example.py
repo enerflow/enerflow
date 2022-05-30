8548687f6d49b92191169dcfec529fdfa5412f90
dimili <i.g.dimoulkas@gmail.com>
2022-05-30 15:50:26 +0200

    Add example

---
+++ examples/heat-demand-forecasting/heat_demand_forecasting_example.py
@@ -0,0 +1,23 @@
+import json
+import pandas as pd
+from enerflow_forecast import forecast
+
+
+# Import data
+data = pd.read_csv('./examples/heat-demand-forecasting/data.csv', header=0, index_col=0, parse_dates=True)
+
+
+# Import parameters
+params_path = './examples/heat-demand-forecasting/parameters.json'
+with open(params_path, 'r', encoding='utf-8') as file:
+    params = json.loads(file.read())
+    
+
+# Add site name in the dataframe to comply with the data input structure
+site_name = params['sites'][0]
+data.columns = pd.MultiIndex.from_product([[site_name], data.columns])
+
+
+# Create and train model    
+trial = forecast.Trial(params)
+score_train_model, score_valid_model = trial.run_pipeline(data)
